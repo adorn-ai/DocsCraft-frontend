22d1d416c172df307cc1522556428037828e123b
unknown <denzelkinyua11@gmail.com>
2025-12-05 01:11:49 +0300

    Commit: Remove sign in with Google for some time

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -30,12 +30,14 @@ export default function Login() {
             Continue with GitHub
           </Button>
           
+          {/*
           <Button 
             onClick={signInWithGoogle} 
             className="w-full bg-white hover:bg-gray-50 text-gray-900 border-2" 
             size="lg"
             variant="outline"
           >
+            
             <svg className="mr-2 h-5 w-5" viewBox="0 0 24 24">
               <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
               <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
@@ -44,6 +46,7 @@ export default function Login() {
             </svg>
             Continue with Google
           </Button>
+          */}
 
           <p className="text-xs text-center text-gray-500 mt-6">
             By continuing, you agree to our Terms of Service and Privacy Policy
